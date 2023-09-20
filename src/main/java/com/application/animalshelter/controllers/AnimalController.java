63b5ef5a2b49a1fb5d7167302887428c4bdf5166
guritch <guritch1@mail.ru>
2023-09-20 18:36:08 +0300

    add doc api swagger

--- src/main/java/com/application/animalshelter/controllers/AnimalController.java
+++ src/main/java/com/application/animalshelter/controllers/AnimalController.java
@@ -2,6 +2,15 @@ package com.application.animalshelter.controllers;
 
 import com.application.animalshelter.entıty.Animal;
 import com.application.animalshelter.service.AnimalService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,21 +26,75 @@ public class AnimalController {
         this.animalService = animalService;
     }
 
+    @Operation(summary = "ДОБАВИТЬ ЖИВОТНОЕ",
+            responses = {@ApiResponse(
+                    responseCode = "200",
+                    description = "Животное добавлено",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = Animal.class)
+                    )
+            )
+            },
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Введите объект в формате JSON"
+            ),
+            tags = "Животные"
+    )
     @PostMapping()
-    public ResponseEntity<Animal> addAnimal (@RequestBody Animal animal){
+    public ResponseEntity<Animal> addAnimal(@RequestBody Animal animal) {
         return ResponseEntity.ok(animalService.addAnimal(animal));
     }
+
+    @Operation(summary = "УДАЛИТЬ ПО ID",
+            responses = {@ApiResponse(
+                    responseCode = "200",
+                    description = "Животное удалено",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = Animal.class)
+                    )
+            )
+            },
+            tags = "Животные"
+    )
     @DeleteMapping()
-    public ResponseEntity<Animal> deleteAnimal (@RequestParam Long id){
+    public ResponseEntity<Animal> deleteAnimal(@Parameter(description = "ID животного", example = "1") @RequestParam Long id) {
         animalService.deleteAnimalById(id);
         return ResponseEntity.ok().build();
     }
+
+    @Operation(summary = "ПОЛУЧИТЬ ПО ID",
+            responses = {@ApiResponse(
+                    responseCode = "200",
+                    description = "Получено животное",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = Animal.class)
+                    )
+            )
+            },
+            tags = "Животные"
+    )
     @GetMapping()
-    public ResponseEntity<Optional<Animal>> getAnimal(@RequestParam Long id){
+    public ResponseEntity<Optional<Animal>> getAnimal(@Parameter(description = "ID животного", example = "1") @RequestParam Long id) {
         return ResponseEntity.ok(animalService.findAnimal(id));
     }
+
+    @Operation(summary = "ПОЛУЧИТЬ ВСЕХ ЖИВОТНЫХ",
+            responses = {@ApiResponse(
+                    responseCode = "200",
+                    description = "Полученные животные",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            array = @ArraySchema(schema = @Schema(implementation = Animal.class))
+                    )
+            )
+            },
+            tags = "Животные"
+    )
     @GetMapping("/getAll")
-    public ResponseEntity<Collection<Animal>> getAllAnimals(){
+    public ResponseEntity<Collection<Animal>> getAllAnimals() {
         return ResponseEntity.ok(animalService.getAllAnimal());
     }
 }
--- src/main/java/com/application/animalshelter/controllers/ShelterController.java
+++ src/main/java/com/application/animalshelter/controllers/ShelterController.java
@@ -1,7 +1,15 @@
 package com.application.animalshelter.controllers;
 
+import com.application.animalshelter.entıty.Animal;
 import com.application.animalshelter.entıty.Shelter;
 import com.application.animalshelter.service.ShelterService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +25,21 @@ public class ShelterController {
         this.shelterService = shelterService;
     }
 
+    @Operation(summary = "ДОБАВИТЬ ПРИЮТ",
+            responses = {@ApiResponse(
+                    responseCode = "200",
+                    description = "Приют добавлен",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = Animal.class)
+                    )
+            )
+            },
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Введите объект в формате JSON"
+            ),
+            tags = "Приюты"
+    )
     @PostMapping()
     public ResponseEntity<Shelter> addNewShelter(@RequestBody Shelter shelter){
         if(shelter ==null){
@@ -27,8 +50,24 @@ public class ShelterController {
         return ResponseEntity.ok(savedShelter);
     }
 
+    @Operation(summary = "ПОЛУЧИТЬ ПО ID",
+            responses = {@ApiResponse(
+                    responseCode = "200",
+                    description = "Получен приют",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = Animal.class)
+                    )
+            ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Приют не найден в БД"
+                    )
+            },
+            tags = "Приюты"
+    )
     @GetMapping()
-    public ResponseEntity<Shelter> getShelter(@RequestParam Long id){
+    public ResponseEntity<Shelter> getShelter(@Parameter(description = "ID приюта", example = "1")@RequestParam Long id){
         Optional<Shelter> shelter = shelterService.getShelter(id);
         if(shelter.isEmpty()){
             return ResponseEntity.notFound().build();
@@ -36,12 +75,36 @@ public class ShelterController {
         return ResponseEntity.ok(shelter.get());
     }
 
+    @Operation(summary = "УДАЛИТЬ ПО ID",
+            responses = {@ApiResponse(
+                    responseCode = "200",
+                    description = "Приют удален",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = Animal.class)
+                    )
+            )
+            },
+            tags = "Приюты"
+    )
     @DeleteMapping()
     public ResponseEntity<String> deleteShelter(@RequestBody Shelter shelter){
         shelterService.deleteShelter(shelter);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "ПОЛУЧИТЬ ВСЕ ПРИЮТЫ",
+            responses = {@ApiResponse(
+                    responseCode = "200",
+                    description = "Полученные приюты",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            array = @ArraySchema(schema = @Schema(implementation = Animal.class))
+                    )
+            )
+            },
+            tags = "Приюты"
+    )
     @GetMapping("/getAll")
     public ResponseEntity<Collection<Shelter>> getAllShelters(){
         return ResponseEntity.ok(shelterService.getAllShelters());
